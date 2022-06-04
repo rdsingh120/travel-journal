3e1c5653bd56f6967fe188cae3513ee2a5b6057d
rdsingh120 <82333889+rdsingh120@users.noreply.github.com>
2022-06-04 17:12:39 -0300

    created data.js, data added to data.js

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        title: 'Christ the Redeemer',
+        location: 'Brazil',
+        googleMapsUrl: 'https://www.google.com/maps/place/Christ+the+Redeemer/@-22.951916,-43.2104872,15z/data=!4m5!3m4!1s0x0:0x9dc984d7019502de!8m2!3d-22.951916!4d-43.2104872',
+        startDate: 'Feb 15,2021',
+        endDate: 'Feb 21,2021',
+        description: 'Christ the Redeemer, colossal statue of Jesus Christ at the summit of Mount Corcovado, Rio de Janeiro, southeastern Brazil. Celebrated in traditional and popular songs, Corcovado towers over Rio de Janeiro, Brazil’s principal port city. The statue of Christ the Redeemer was completed in 1931 and stands 98 feet (30 metres) tall, its horizontally outstretched arms spanning 92 feet (28 metres). The statue has become emblematic of both the city of Rio de Janeiro and the whole nation of Brazil.',
+        imageUrl: 'https://images.unsplash.com/photo-1593995863951-57c27e518295?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+    },
+    {
+        title: 'Niagara Falls',
+        location: 'Canada',
+        googleMapsUrl: 'https://www.google.com/maps/place/Niagara+Falls,+ON/data=!4m2!3m1!1s0x89d3445eec824db9:0x46d2c56156bda288?sa=X&ved=2ahUKEwj1g4jiy5T4AhWrUjABHVCnAosQ8gF6BAhiEAE',
+        startDate: 'Mar 01,2021',
+        endDate: 'Mar 05,2021',
+        description: 'Niagara Falls, waterfall on the Niagara River in northeastern North America, one of the continent’s most famous spectacles. The falls lie on the border between Ontario, Canada, and New York state, U.S. For many decades the falls were an attraction for honeymooners and for such stunts as walking over the falls on a tightrope or going over them in a barrel. Increasingly, however, the appeal of the site has become its beauty and uniqueness as a physical phenomenon.',
+        imageUrl: 'https://images.unsplash.com/photo-1559344465-ac6ad8e59943?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+    },
+    {
+        title: 'Burj Khalifa',
+        location: 'United Arab Emirates',
+        googleMapsUrl: 'https://www.google.com/maps/place/Burj+Khalifa/@25.197197,55.2743764,15z/data=!4m5!3m4!1s0x0:0xff45e502e1ceb7e2!8m2!3d25.197197!4d55.2743764',
+        startDate: 'Apr 21,2022',
+        endDate: 'Apr 28,2022',
+        description: 'Burj Khalifa, mixed-use skyscraper in Dubai, United Arab Emirates, that is the world’s tallest building, according to all three of the main criteria by which such buildings are judged (see Researcher’s Note: Heights of Buildings). Burj Khalifa (“Khalifa Tower”), known during construction as Burj Dubai, was officially named to honour the leader of the neighbouring emirate of Abu Dhabi, Sheikh Khalifa ibn Zayed Al Nahyan. Although the tower was formally opened on January 4, 2010, the entirety of the interior was not complete at that time. Built to house a variety of commercial, residential, and hospitality ventures, the tower—whose intended height remained a closely guarded secret throughout its construction—reached completion at 162 floors and a height of 2,717 feet (828 metres). It was designed by the Chicago-based architectural firm of Skidmore, Owings & Merrill. Adrian Smith served as architect, and William F. Baker served as structural engineer.',
+        imageUrl: 'https://images.unsplash.com/photo-1602205265393-06b5d1ee8ab7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80'
+    }
+]
+
+export default data;
